3de7e5cd0bc2e3a65327517ca4675de4ebcfbc6f
RyanCornelio <ryan.cornelio@gmail.com>
2023-02-13 16:38:36 +0100

    responsive youtube

--- src/Pages/Ryan.jsx
+++ src/Pages/Ryan.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
 import "./Ryan.css";
+import ry3 from "../Img/ry3.png"
+import ry2 from "../Img/ry2.png"
+import ry1 from "../Img/ry1.png"
+import React, { Component } from 'react'
+import Typical from 'react-typical'
 
 const Ryan = () => {
   return (
     <>
-    <div className="header">
 
-    
-    </div>
-    <div className='om'>
-    <p>Fotograf: Ryan</p>
-    <div><img src="/src/Img/ryan.jpg" alt="" /></div>
-    </div>
-    
-    
- 
+      <h3 className="banner">Webbutveckling</h3>
+      <div className="about-container">
+
+        <div className="photo-box">
+          <img src={ry2} alt="" />
+          <div className="text">
+            <h1 className="name">Ryan-Phillips Iserhills Cornelio</h1>
+            <span>Hej! Som ni ser så gillar jag att träna! Men även att 
+              fotografera och spela in videos. Till en början var tanken att fota på semester men sen kom det en pandemi. Och det är kul att fota annat med. 
+              <br />
+              <br />
+              Jag söker jobb inom webbutveckling och har gjort denna sidan i React.JS.
+            </span>
+
+          </div>
+        </div>
+      </div>
+      
     </>
   )
 }
--- src/components/Galleri.jsx
+++ src/components/Galleri.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Galleri = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <h3 className="banner">Film</h3>
+      </div>
+      <div>
+        
+      </div>
+    </>
   )
 }
 
