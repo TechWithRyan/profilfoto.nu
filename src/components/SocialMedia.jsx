e9503fd5ec78704ece8306253f304f877bff9acf
RyanCornelio <ryan.cornelio@gmail.com>
2023-02-17 16:47:05 +0100

    Fixed social media list

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -8,23 +8,23 @@ const SocialMedia = () => {
     <div id="sidebar">
   <div className="social facebook">
     <a href=" https://www.facebook.com/ryanphillips.cornelio/ " target="_blank">
-      <p>Like on Facebook <i className="fa fa-facebook "></i> </p>
+      <p>Facebook <i className="fa fa-facebook "></i> </p>
     </a>
   </div>
   <div className="social github">
     <a href=" https://github.com/TechWithRyan " target="_blank">
-      <p>Follow on Github<i className="fa fa-github"></i> </p>
+      <p>Github<i className="fa fa-github"></i> </p>
     </a>
   </div>
   
   <div className="social youtube">
     <a href=" https://www.youtube.com/channel/UCOg3PF76vT3K6DMGcU7-XzA " target="_blank">
-      <p>Subscribe on Youtube<i className="fa fa-youtube-play"></i> </p>
+      <p>Youtube<i className="fa fa-youtube-play"></i> </p>
     </a>
   </div>
   <div className="social linkedin">
     <a href=" https://www.linkedin.com/in/ryan-phillips-iserhills-cornelio/ " target="_blank">
-      <p>Connect on LinkedIn<i className="fa fa-linkedin"></i> </p>
+      <p>LinkedIn<i className="fa fa-linkedin"></i> </p>
     </a>
   </div>
 </div>
